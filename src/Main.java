aa90cf47e72fab42ee7268e84dc10fc23d9e18fc
Raphael_Pinho <raphapinho7@gmail.com>
2026-01-06 15:12:15 -0300

    criando uma calculadora utilizando enum

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,38 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
-import domain.User;
+import java.util.Scanner;
 
+import enumeration.*;
 public class Main {
     public static void main(String[] args) {
-        Map<String, User> users = new LinkedHashMap<>();
-        users.put("joao@joao.com", new User("joao", 22));
-        users.put("maria@maria.com", new User("Maria", 32));
-        users.put("juca@juca.com", new User("Juca", 18));
-        users.put("leo@leo.com", new User("Leo", 48));
+        var scanner = new Scanner(System.in);
 
-        users.merge("leo@leo.com", new User("", -1), (user, user2) -> {
-            System.out.println(user);
-            System.out.println(user2);
-            return user2;
-        });
-        System.out.println(users);
+    var option = -1;
+    while (option != 5) {
+        System.out.println("Escolha uma opção:");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+        System.out.println("5 - Sair");
+        option = scanner.nextInt();
+
+
+        if (option > 5 || option < 1) {
+            System.out.println("Selecione uma opção valida");
+            continue;
+        }
+        if (option == 5) {
+            break;
+        }
+        var selectedOption = OperationEnum.values()[option - 1];
+
+        System.out.println("Informe o primeiro valor");
+        var value1 = scanner.nextInt();
+        System.out.println("Informe o segundo valor");
+        var value2 = scanner.nextInt();
+
+        var result = selectedOption.getCalculate().apply(value1, value2);
+
+        System.out.printf("%s %s %s = %s\n",value1, selectedOption.getSymbol(), value2, result);
+    }
     }
 }
--- src/domain/User.java
+++
@@ -1,5 +0,0 @@
-package domain;
-
-public record User(String name, int age) {
-    
-}
